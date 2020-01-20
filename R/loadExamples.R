62cc356337b8c09fcfa2225bb2d2bbf3e1b2adc4
Frantisek Malinka <frantisek.malinka@gmail.com>
2020-01-20 11:18:55 +0100

    bioconductor notes

--- R/loadExamples.R
+++ R/loadExamples.R
@@ -11,41 +11,41 @@ mystruct <- list()
 if(dotOBOpath == "" || goOBOpath == "" || mymatrixpath == "" ||
 initColpath == "" || initRowpath =="")
 {
-  mystruct <- NULL
+mystruct <- NULL
 }
 else
 {
-  mystruct[["colOntologyPath"]] <- dotOBOpath
-  mystruct[["rowOntologyPath"]] <- goOBOpath
-  mymatrix <- read.csv(mymatrixpath, stringsAsFactors = FALSE)
-  rownames(mymatrix) <- mymatrix[,1]
-  mymatrix <- mymatrix[,-1]
-  mystruct[["datamatrix"]] <- as.matrix(mymatrix)
-  initCol <- read.csv(initColpath, stringsAsFactors = FALSE, header = FALSE)
-  initRow <- read.csv(initRowpath, stringsAsFactors = FALSE, header = FALSE)
-  
-  colList <- list()
-  for(icol in 1:nrow(initCol))
-  {
-    myvct <- as.character(initCol[icol,-1])
-    myvct <- myvct[myvct!=""]
-    colList <- append(colList, list(myvct))
-    names(colList)[icol] <- initCol[icol,1]
-  }
-  mystruct[["colOntologyDesc"]] <- colList
-  
-  rowList <- list()
-  for(irow in 1:nrow(initRow))
-  {
-    myvct <- as.character(initRow[irow,-1])
-    myvct <- myvct[myvct!=""]
-    rowList <- append(rowList, list(myvct))
-    names(rowList)[irow] <- initRow[irow,1]
-  }
-  mystruct[["rowOntologyDesc"]] <-  rowList
+mystruct[["colOntologyPath"]] <- dotOBOpath
+mystruct[["rowOntologyPath"]] <- goOBOpath
+mymatrix <- read.csv(mymatrixpath, stringsAsFactors = FALSE)
+rownames(mymatrix) <- mymatrix[,1]
+mymatrix <- mymatrix[,-1]
+mystruct[["datamatrix"]] <- as.matrix(mymatrix)
+initCol <- read.csv(initColpath, stringsAsFactors = FALSE, header = FALSE)
+initRow <- read.csv(initRowpath, stringsAsFactors = FALSE, header = FALSE)
+
+colList <- list()
+for(icol in 1:nrow(initCol))
+{
+myvct <- as.character(initCol[icol,-1])
+myvct <- myvct[myvct!=""]
+colList <- append(colList, list(myvct))
+names(colList)[icol] <- initCol[icol,1]
+}
+mystruct[["colOntologyDesc"]] <- colList
+
+rowList <- list()
+for(irow in 1:nrow(initRow))
+{
+myvct <- as.character(initRow[irow,-1])
+myvct <- myvct[myvct!=""]
+rowList <- append(rowList, list(myvct))
+names(rowList)[irow] <- initRow[irow,1]
+}
+mystruct[["rowOntologyDesc"]] <-  rowList
 }
 
- return(mystruct)
+return(mystruct)
 }
 
 
